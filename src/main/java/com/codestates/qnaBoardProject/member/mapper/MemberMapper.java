8d1943a8588e2d9988d79c8df9073177a0faab2b
Kyungju <wwmix@naver.com>
2023-04-26 23:03:47 +0900

    modify) MemberMapper - memberPostDto 매개변수가 다른 문제 해결

--- src/main/java/com/codestates/qnaBoardProject/member/mapper/MemberMapper.java
+++ src/main/java/com/codestates/qnaBoardProject/member/mapper/MemberMapper.java
@@ -1,13 +1,14 @@
 package com.codestates.qnaBoardProject.member.mapper;
 
 import com.codestates.qnaBoardProject.member.dto.MemberPatchDto;
+import com.codestates.qnaBoardProject.member.dto.MemberPostDto;
 import com.codestates.qnaBoardProject.member.dto.MemberResponseDto;
 import com.codestates.qnaBoardProject.member.entity.Member;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
-    Member memberPostDtoToMember(MemberPatchDto memberPatchDto);
+    Member memberPostDtoToMember(MemberPostDto memberPostDto);
     Member memberPatchDtoToMember(MemberPatchDto memberPatchDto);
     MemberResponseDto memberToMemberResponseDto(Member member);
 }
