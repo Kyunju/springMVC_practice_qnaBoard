2548e45dac09218e3beeb47d330fef4c0441cee3
Kyungju <wwmix@naver.com>
2023-04-26 22:59:16 +0900

    modify) MemberService 비지니스 로직 구현

--- src/main/java/com/codestates/qnaBoardProject/member/service/MemberService.java
+++ src/main/java/com/codestates/qnaBoardProject/member/service/MemberService.java
@@ -1,34 +1,77 @@
 package com.codestates.qnaBoardProject.member.service;
 
+import com.codestates.qnaBoardProject.exception.BusinessLogicException;
+import com.codestates.qnaBoardProject.exception.ExceptionCode;
 import com.codestates.qnaBoardProject.member.entity.Member;
+import com.codestates.qnaBoardProject.member.repository.MemberRepository;
+import com.codestates.qnaBoardProject.stamp.Stamp;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public Member createMember(Member member) {
-        // TODO: 기존 멤버가 존재하는지 조회
+        // 기존 멤버가 존재하는지 조회
+        verifyExistEmail(member.getEmail());
+
+        // stamp 추가
+        initStamp(member);
 
         // 기존멤버가 존재하지 않는다면 데이터베이스에 추가
-        return null;
+        return memberRepository.save(member);
     }
     public Member updateMember(Member member) {
-        // TODO: 기존 멤버 가져오기
+        // 기존 멤버 가져오기
+        Member findMember = findVerifiedMember(member.getMemberId());
 
         // null 이 아닌 부분만 가져온 멤버에 수정
+        Optional.ofNullable(member.getName())
+                .ifPresent(name -> member.setName(name));
+        Optional.ofNullable(member.getPhone())
+                .ifPresent(phone -> member.setPhone(phone));
+        Optional.ofNullable(member.getMemberStatus())
+                .ifPresent(memberStatus -> member.setMemberStatus(memberStatus));
+        findMember.setModifiedAt(LocalDateTime.now());
 
         // 데이터베이스에 수정된 Member 엔티티 저장
-        return null;
+        return memberRepository.save(findMember);
     }
 
     public Member findMember(long memberId) {
-        // TODO: memberId를 통해서 member 가져오기
+        // memberId를 통해서 member를 리턴
 
-        // 가져온 멤버를 리턴
-        return null;
+        return findVerifiedMember(memberId);
     }
 
     public void deleteMember(long memberId) {
-        // TODO : memberID를 통해서 멤버 가져오기
+        // memberID를 통해서 멤버 가져오기
+        Member findMember = findVerifiedMember(memberId);
         // 멤버가 존재하면 해당 멤버를 데이터베이스에서 삭제
+        memberRepository.delete(findMember);
+    }
+
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(
+                        () -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
+    }
+
+    private void verifyExistEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent())
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+    }
+    private void initStamp(Member member) {
+        member.setStamp(new Stamp());
     }
 }
