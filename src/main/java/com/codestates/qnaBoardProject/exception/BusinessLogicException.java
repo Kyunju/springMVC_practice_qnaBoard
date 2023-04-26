3782b354f0a14af2011d23261cb7a197800433c6
Kyungju <wwmix@naver.com>
2023-04-26 22:31:02 +0900

    add) BusinessLogicException 추가

---
+++ src/main/java/com/codestates/qnaBoardProject/exception/BusinessLogicException.java
@@ -0,0 +1,12 @@
+package com.codestates.qnaBoardProject.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException{
+    @Getter
+    private ExceptionCode exceptionCode;
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
