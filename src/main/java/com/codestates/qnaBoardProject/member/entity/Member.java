ac35a6f9b20814286714e793a7ded58bab605d86
Kyungju <wwmix@naver.com>
2023-04-26 17:45:07 +0900

    add) Member Entity 추가

---
+++ src/main/java/com/codestates/qnaBoardProject/member/entity/Member.java
@@ -0,0 +1,53 @@
+package com.codestates.qnaBoardProject.member.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(length = 13, nullable = false, unique = true)
+    private String phone;
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    public Member(String email) {
+        this.email = email;
+    }
+
+    public Member(String email, String name, String phone) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
+}
