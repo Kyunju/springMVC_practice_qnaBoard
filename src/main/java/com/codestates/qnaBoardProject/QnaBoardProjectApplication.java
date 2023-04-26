4b4520f23cac53e366f911d6fff0c8effb2ca56b
Kyungju <wwmix@naver.com>
2023-04-26 16:27:47 +0900

    build project

---
+++ src/main/java/com/codestates/qnaBoardProject/QnaBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.qnaBoardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaBoardProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/qnaBoardProject/QnaBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.qnaBoardProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
