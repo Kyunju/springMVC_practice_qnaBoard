3453ef1bcadcd2a4c02375cd955595132c7eab2d
Kyungju <wwmix@naver.com>
2023-04-28 00:35:35 +0900

    add) Qna vis 엔티티 visibility 상태 추가

--- src/main/java/com/codestates/qnaBoardProject/board/entity/Qna.java
+++ src/main/java/com/codestates/qnaBoardProject/board/entity/Qna.java
@@ -26,6 +26,9 @@ public class Qna {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private QnaStatus qnaStatus = QnaStatus.QUESTION_REGISTERED;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private QnaVisibility qnaVisibility = QnaVisibility.QUESTION_PUBLIC;
 
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
@@ -34,14 +37,29 @@ public class Qna {
     // TODO: 답변 엔티티랑 연관관계 맵핑
 
     public enum QnaStatus {
-        QUESTION_REGISTERED("질문 등록"),
-        QUESTION_ANSWERED("답변 완료"),
-        QUESTION_DELETED("질문 삭제"),
-        QUESTION_DEACTIVATED("질문 비활성화");
+        QUESTION_REGISTERED(1, "질문 등록"),
+        QUESTION_ANSWERED(2, "답변 완료"),
+        QUESTION_DELETED(3, "질문 삭제"),
+        QUESTION_DEACTIVATED(4, "질문 비활성화");
+        @Getter
+        private int stepNumber;
+        @Getter
         private String status;
 
-        QnaStatus(String status) {
+        QnaStatus(int stepNumber, String status) {
+            this.stepNumber = stepNumber;
             this.status = status;
+        }
+    }
+
+    public enum QnaVisibility {
+        QUESTION_PUBLIC("공개"),
+        QUESTION_SECRET("비공개");
+        @Getter
+        private String visibility;
+
+        QnaVisibility(String visibility) {
+            this.visibility = visibility;
         }
     }
 
