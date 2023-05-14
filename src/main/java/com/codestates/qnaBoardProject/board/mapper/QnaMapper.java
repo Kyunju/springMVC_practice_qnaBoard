e08ba6bf8d976c107d7976075a148d9a7bb4f55c
Kyungju <wwmix@naver.com>
2023-05-15 05:40:04 +0900

    modify) QnaMapper qna to response 작성자 맵핑 수정

--- src/main/java/com/codestates/qnaBoardProject/board/mapper/QnaMapper.java
+++ src/main/java/com/codestates/qnaBoardProject/board/mapper/QnaMapper.java
@@ -5,6 +5,7 @@ import com.codestates.qnaBoardProject.board.dto.QnaPostDto;
 import com.codestates.qnaBoardProject.board.dto.QnaResponseDto;
 import com.codestates.qnaBoardProject.board.entity.Qna;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.awt.*;
 
@@ -12,5 +13,6 @@ import java.awt.*;
 public interface QnaMapper {
     Qna qnaPostDtoToQna(QnaPostDto qnaPostDto);
     Qna qnaPatchDtoToQna(QnaPatchDto qnaPatchDto);
+    @Mapping(target = "createdBy", source = "member.name")
     QnaResponseDto qnaToQnaResponseDto(Qna qna);
 }
