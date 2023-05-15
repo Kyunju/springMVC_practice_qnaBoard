7e0130cf00ceec1b1ba12db7cf922dea1e496017
Kyungju <wwmix@naver.com>
2023-05-15 23:22:38 +0900

    add) admin 지정 클래스 추가

---
+++ src/main/java/com/codestates/qnaBoardProject/admin/Admin.java
@@ -0,0 +1,10 @@
+package com.codestates.qnaBoardProject.admin;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class Admin {
+    private final String EMAIL = "admin@gmail.com";
+}
